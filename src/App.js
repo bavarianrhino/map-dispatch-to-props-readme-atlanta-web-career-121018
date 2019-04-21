9bbcf874dd54fe9c8737cc85235ffb08acf9b7d1
github username <github email address>
2019-04-21 08:43:21 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
-import { addItem } from  './actions/items';
+import { addItem } from './actions/items';
 
 class App extends Component {
+	handleOnClick = (e) => {
+		this.props.addItem();
+	};
 
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
-          Click
-          </button>
-        <p>{this.props.items.length}</p>
-      </div>
-    );
-  }
-};
+	render() {
+		return (
+			<div className='App'>
+				<button onClick={(event) => this.handleOnClick(event)}>Click</button>
+				<p>{this.props.numberOfItems}</p>
+				<p>Random Count that Ryan Added: {this.props.count}</p>
+			</div>
+		);
+	}
+}
 
 const mapStateToProps = (state) => {
-  return {
-    items: state.items
-  };
+	return {
+        items: state.items,
+        numberOfItems: state.items.length,
+        count: state.count
+	};
 };
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		addItem: () => {
+			dispatch(addItem());
+		},
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// Can Abstract with below
+// export default connect(mapStateToProps, { addItem })(App);
+
+// Go an extra step and get rid of mapStateToProps function with below
+// export default connect(state => ({ items: state.items }), { addItem })(App);
+
+// ABSTRACT ONE MORE TIME WITH BELOW, DON'T NEED mapDispatchToProps
+// By default mapDispatchToProps is just dispatch => ({ dispatch }).
+// So if you don't specify the second argument to connect(), you'll get dispatch injected as a prop in your component.
+
+// export default connect(state => ({ items: state.items }))(App);
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,17 +1,26 @@
-export default function shoppingListItemReducer(state = {
-  items: []
-}, action) {
-  console.log(action)
-  switch(action.type) {
+export default function shoppingListItemReducer(
+	state = {
+        items: [],
+        count: 0
+	},
+	action,
+) {
+	console.log(action);
+	switch (action.type) {
+		case 'INCREASE_COUNT':
+			console.log(state.items.concat(state.items.length));
+			return Object.assign(
+				{},
+				{
+					items: state.items.concat(state.items.length),
+                },
+                {
+                    count : state.count + 863
+                }
+			);
 
-    case 'INCREASE_COUNT':
-      console.log(state.items.concat(state.items.length));
-      return Object.assign({}, {
-        items: state.items.concat(state.items.length)
-      });
-
-    default:
-      console.log(state)
-      return state;
-  }
-};
+		default:
+			console.log(state);
+			return state;
+	}
+}
